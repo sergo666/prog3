8eca530dd73bb9a42572a948db349a8d57f2cee6
agrmv <gromov-aleksey@outlook.com>
2017-04-20 20:24:25 +0700

    fix main

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include "command.h"
 #include <string.h>
 
-int main(int argc, char *argv[])
+/*int main(int argc, char *argv[])
 {
 
 	if (strcmp("encode", argv[1]) == 0) {
@@ -14,5 +14,34 @@ int main(int argc, char *argv[])
 		printf("Неверен первый аргумент\n");
 	}
 
+	return 0;
+}*/
+
+int main(int argc, char *argv[])
+{
+	
+	if (argc != 4) {
+		printf("Usage:\n"
+		"coder encode <in-file-name> <out-file-name>\n"
+		"coder decode <in-file-name> <out-file-name>\n");
+		return -1;
+	}
+	
+	if(!strcmp(argv[1], "decode")) {
+		if (decode_file(argv[2], argv[3])) {
+			printf("Error decode file\n");
+			return -1;
+		} 
+	} else if(!strcmp(argv[1], "encode")) {
+		if (encode_file(argv[2], argv[3])) {
+			printf("Error encode file\n");
+			return -1;
+		} 
+	} else {
+		printf("Usage:\n"
+		"coder encode <in-file-name> <out-file-name>\n"
+		"coder decode <in-file-name> <out-file-name>\n");
+	}
+	
 	return 0;
 }
