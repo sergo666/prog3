3c712e3960d18e2f0014bf6d603cf85c57bedf45
sergo666 <serrrfff@gmail.com>
2017-04-21 13:54:29 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,19 +3,6 @@
 #include "command.h"
 #include <string.h>
 
-/*int main(int argc, char *argv[])
-{
-
-	if (strcmp("encode", argv[1]) == 0) {
-		encode_file(argv[2], argv[3]);
-	} else if (strcmp("decode", argv[1]) == 0) {
-		decode_file(argv[2], argv[3]);
-	} else {
-		printf("Неверен первый аргумент\n");
-	}
-
-	return 0;
-}*/
 
 int main(int argc, char *argv[])
 {
